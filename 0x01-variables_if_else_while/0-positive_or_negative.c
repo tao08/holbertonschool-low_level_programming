018e84eefe4ffab66423d3e42a12e94ef1f3a0b1
tao08 <1823@holbertonschool.com>
2020-06-11 23:21:12 +0000

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0) 
-printf("%d is positive\n", n);
+printf("%i is positive\n", n);
 else if (n == 0)
-printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negatavive\n", n);
+printf("%i is zero\n", n);
+else
+printf("%i is negatavive\n", n);
 return (0);
 }
