03eaa1688b98338572ebd97d230ecbcbf3e22406
tao08 <1823@holbertonschool.com>
2020-06-15 03:11:40 +0000

    dfd

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Combinations
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int k;
+	int u;
+	for (k = '0'; k <= '9'; k++)
+	{
+		for (u = k + 1; u <= '9'; u++)
+		{
+			putchar(k);
+			putchar(u);
+			if (k != '8' || u != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
