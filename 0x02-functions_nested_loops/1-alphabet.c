f0b72797e20f88506669fd1bd215cbf9b55d5a91
tao08 <1823@holbertonschool.com>
2020-06-16 17:22:09 +0000

    comi

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
+
 /**
  * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
 	int a;
 
-	for(a = 97; a <= 122; a++)
+	for (a = 97; a <= 122; a++)
 {
-		_putchar(a);
+	_putchar(a);
 }
- 	        _putchar('\n');
+	_putchar('\n');
 }
