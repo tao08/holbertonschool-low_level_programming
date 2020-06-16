dd7a2c82988f9c596e1b03e03a9c84f29434380b
tao08 <1823@holbertonschool.com>
2020-06-16 17:35:08 +0000

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * print_alphabet_x10 - check the code for Holberton School students.
  *
@@ -9,12 +10,13 @@ void print_alphabet_x10(void)
 {
 	int b;
 	int a;
+
 	for (b = 0; b <= 9; b++)
 	{
 		for (a = 97; a <= 122; a++)
 		{
-	_putchar(a);
-}
+			_putchar(a);
+		}
 		_putchar('\n');
-}
+	}
 }
