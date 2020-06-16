2144feb5d2248f096625a5c13d240fc38896cbcb
tao08 <1823@holbertonschool.com>
2020-06-16 03:27:20 +0000

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int b;
+	int a;
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+	_putchar(a);
+}
+		_putchar('\n');
+}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _islower - checkers for lowercase char
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int min = 97;
+	int x =122;
+
+        if ((c >= min) && (c <= x))
+	{
+		return (1);
+	}
+	else
+{
+	return (0);
+}
+}
