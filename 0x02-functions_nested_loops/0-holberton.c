6d29225a761cf56c53c8d677e3c622a6646da993
tao08 <1823@holbertonschool.com>
2020-06-16 16:28:00 +0000

    poi

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
-
+/**
+ *
+ *Write a program that prints Holberton, followed by a new line.
+ *
+ */
 int main(void)
 {
-        _putchar('H');
-        _putchar('o');
+
+	_putchar('H');
+	_putchar('o');
 	_putchar('l');
 	_putchar('b');
-        _putchar('e');
+	_putchar('e');
 	_putchar('r');
 	_putchar('t');
 	_putchar('o');
 	_putchar('n');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
-
-
- 
