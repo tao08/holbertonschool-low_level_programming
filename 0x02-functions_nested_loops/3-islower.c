1e6d6790750131d158ca9d7256cf447a622dbcf3
tao08 <1823@holbertonschool.com>
2020-06-16 17:44:54 +0000

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
+
 /**
  * _islower - checkers for lowercase char
  *
  * Return: Always 0.
  */
+
 int _islower(int c)
 {
 	int min = 97;
-	int x =122;
+	int x = 122;
 
-        if ((c >= min) && (c <= x))
+	if ((c >= min) && (c <= x))
 	{
 		return (1);
 	}
 	else
-{
-	return (0);
-}
+	{
+		return (0);
+	}
 }
