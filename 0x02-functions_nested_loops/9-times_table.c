578ac86adf071ef8494dfa2978842c5746417d93
tao08 <1823@holbertonschool.com>
2020-06-17 02:09:22 +0000

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * time_table - print the 9 time table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int columna, fila, nfila, multi, ncol;
+
+	columna = 0;
+
+	while (columna <= 9)
+	{
+		for (fila = 0; fila <= 9; fila++)
+		{
+			multi = columna * fila;
+			ncol = multi / 10;
+			nfila = multi % 10;
+
+			if (multi > 9)
+			{
+				_putchar(ncol + '0');
+				_putchar(nfila + '0');
+			}
+			else
+			{
+				if (fila >= 1)
+				{
+					_putchar(' ');
+					_putchar(nfila + '0');
+				}
+				else
+				{
+					_putchar(nfila + '0');
+				}
+			}
+			if (fila != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		columna++;
+		_putchar('\n');
+	}
+}
