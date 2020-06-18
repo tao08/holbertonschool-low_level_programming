0ef60c52ef8165e7057435bd4c51f0da7008f1c7
tao08 <1823@holbertonschool.com>
2020-06-18 03:22:31 +0000

    commit

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int main(void);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remainig_days(int month,int day,in yera);
+void print_to_98(int n);
 
 #endif /* HOLBERTON_H */
