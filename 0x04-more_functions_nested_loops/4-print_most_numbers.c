98a384b9db5aea586e9bb5ca64f9fb6890f2899e
tao08 <1823@holbertonschool.com>
2020-06-18 23:43:58 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers  from 0 to 9, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int g;
+
+	fot (g = '0'; g <= '9'; g++)
+	{
+		if (g != '2' && g != '4')
+
+			_putchar(g);
+	}
+	_putchar('\n');
+}
