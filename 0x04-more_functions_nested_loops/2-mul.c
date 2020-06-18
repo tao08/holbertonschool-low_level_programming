d832814605f7545b2fc455ca852624b750b75bb4
tao08 <1823@holbertonschool.com>
2020-06-18 23:14:43 +0000

    comi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *mul - check the code
+ *@a: check the code
+ *@b: check the code
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
