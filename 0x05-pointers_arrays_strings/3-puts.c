0006ca0b018f7babfd93ba20e2ba09e7b7931a46
tao08 <1823@holbertonschool.com>
2020-06-23 23:15:27 +0000

    añadi cero y holberton

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,16 @@
 #include "holberton.h"
 /**
-* _puts - return the longitud
-*
-* @str: this is pointer
-*
-* Return: Always 0.
-*/
+ * _puts -  function that prints a string, followed by a new line, to stdout.
+ * @str: this is pointer
+ * Return: Always 0.
+ **/
 void _puts(char *str)
 {
 	int g;
-
-	g = 0;
 
 	for (g = 0; str[g] != '\0'; g++)
 	{
 		_putchar(str[g]);
 	}
-	_putchart('\n');
+	_putchar('\n');
 }
