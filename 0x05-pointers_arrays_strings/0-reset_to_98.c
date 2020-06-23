39970afca92cff1d081e3d8abfd469749ac45ed0
tao08 <1823@holbertonschool.com>
2020-06-23 17:08:58 +0000

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - check the code for Holberton School.
+ * @n:this is pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+* Structs,  enums and unions definitions
+* Typedef
+* Function prototypes
+*/
+
+void reset_to_98(int *n);
+
+
+#endif /* HOLBERTON_H */
