56b3de5e7b8cb8fc50d99ecd0c352199afdd88b4
tao08 <1823@holbertonschool.com>
2020-06-24 01:01:41 +0000

    reverza

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+ /**
+  * print_rev - prints a string in reverse
+  * @s: this is a punter
+  * Return: Always 0.
+  *
+  **/
+void print_rev(char *s)
+{
+	int g = 0;
+	int t;
+
+	while (s[g] != '\0')
+	{
+		t++;
+	}
+	for (t = g - 1; t >= 0; t--)
+	{
+		_putchar(s[g]);
+	}
+	_putchar('\n');
+}
