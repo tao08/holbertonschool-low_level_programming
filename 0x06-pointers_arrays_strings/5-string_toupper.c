4d82ba31aeeea1b34d61d2f27ec8d88c09d6487b
tao08 <1823@holbertonschool.com>
2020-06-26 04:22:26 +0000

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - Convert a string lower to upper
+ * @s: A string
+ * Return: S
+ */
+char *string_toupper(char *s)
+{
+	int g = 0;
+
+	while (s[g] != '\0')
+	{
+		if (s[g] >= 97 && s[g] <= 122)
+		{
+			s[g] = s[g] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
