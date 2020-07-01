13d4cdd3d49f1b13871415fd4f094e50a788d3ee
tao08 <1823@holbertonschool.com>
2020-07-01 21:52:15 +0000

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,14 @@
  **/
 void _puts_recursion(char *s)
 {
-
-
-        if (*s != '\0')
-	{    s++
-		_putchar(s);
-		_puts_recursion (s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
 	if  (*s == '\0')
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
