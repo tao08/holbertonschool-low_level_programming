0ec90cc4a3fd7f13969fbfa7df11048efd99deed
tao08 <1823@holbertonschool.com>
2020-07-02 04:17:08 +0000

    commit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ * _pow_recursion - Write a function that returns the value of x raised to.
+ * @x: this is a int
+ * @y: this is a int
+ * Return: 1
  **/
 int _pow_recursion(int x, int y)
 {
