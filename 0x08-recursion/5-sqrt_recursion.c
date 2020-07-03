004b0871cac43d4bfce792e2ec97be8104d1b985
tao08 <1823@holbertonschool.com>
2020-07-03 02:24:34 +0000

    comi

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,30 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - this is a recursion
+ * _cuadrada - this is a funtion for the recurtion
  * @g: this is a variable
  * @w: this is a variable
  * Return: -1
  */
-
 int _cuadrada(int g, int w)
 {
 	int raiz;
 
 	if (w * w < g)
 	{
-		raiz = _cuadrada(g, w +1);
+		raiz = _cuadrada(g, w + 1);
 		return (raiz);
 	}
 	if (w * w == g)
-        {
+	{
 		return (w);
 	}
 	return (-1);
-
 }
+/**
+ * _sqrt_recursion - this is a recursion
+ * @n: this is a variable
+ * Return: _cuadrada
+ **/
 int _sqrt_recursion(int n)
 {
 	return (_cuadrada(n, 1));
