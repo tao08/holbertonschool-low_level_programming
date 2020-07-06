486f4c8360bee984d051c8980fbefbcc3cef3457
tao08 <1823@holbertonschool.com>
2020-07-06 04:47:42 +0000

    argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is a funtion
+ * @argc: es un contador de cantidad argumento
+ * @argv: es un contrador de valor de argumentos
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+		printf("%i\n", argc - 1);
+
+		return (0);
+}
