c920ba1bde1b1bcf69d3c817d008308fb554c94a
tao08 <1823@holbertonschool.com>
2020-07-06 04:15:18 +0000

    commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * main - this is a funtion
+ * @argc: this is a varia
+ * @argv: this is a variable
+ * Return: 0.
  **/
-int main (int argc, char *argv [])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
