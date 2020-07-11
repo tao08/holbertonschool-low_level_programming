a4a208ef8470cdc27490567948945dffaeca097a
tao08 <1823@holbertonschool.com>
2020-07-11 00:03:19 +0000

    nimax

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - This will create an array of integers
+ * @min: min number alloed in the array
+ * @max: max number alloed in the array
+ * Return: superptr pointer of the array
+ */
+int *array_range(int min, int max)
+{
+	int g;
+	int w;
+	int *ptr;
+
+	if (min > max)
+	{
+		return ('\0');
+	}
+	w = max - min + 1;
+	ptr = malloc(j * sizeof(int));
+	if (ptr == '\0')
+		return ('\0');
+	for (g = 0; g < w; g++, min++)
+		ptr[g] =  min;
+	return (ptr);
+}
