41e30ffced0cfd04cf7085c1ef4f1aa268da0cb0
tao08 <1823@holbertonschool.com>
2020-07-15 04:02:36 +0000

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - funtion iniciali
+ * @d: puntero struct
+ * @name: puntero tipe char
+ * @owner: variabre propietario
+ *
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
