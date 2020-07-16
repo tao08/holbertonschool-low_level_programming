58af926b29ea148d7663de70c151270f1c44dc5b
tao08 <1823@holbertonschool.com>
2020-07-16 02:03:53 +0000

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - this is a function
+ * @array: this is a punter
+ * @size: this is a punter
+ * @cmp: this is a puntero
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int g, aux = 0;
+
+	if (array && cmp && size >= 0)
+	{
+		for (g = 0; g < size; g++)
+		{
+			aux = cmp(array[g]);
+			if (aux)
+				return (g);
+		}
+
+	}
+	return (-1);
+}
