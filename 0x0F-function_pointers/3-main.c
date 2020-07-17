cef821ae3131b4c4b277c7da3db4e2d120485c96
tao08 <1823@holbertonschool.com>
2020-07-17 15:22:20 +0000

    voi

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,18 +1,25 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "3-calc.h"
 
-int main(int argc, chart **argv)
+/**
+ * main - calcula dos variables tipo entero y char
+ * @argc: variable tipo entero
+ * @argv: variable tipo char
+ * Return: 0
+ **/
+int main(int argc, char **argv)
 {
-
-if ( argc != 4)
-printf("Error\n");
-exit(98);
-
-if (((argv[2]  == '/' || argv[2] '%') && atoi(argv[3]) == 0 ))
-	printf ("Error\n");
-exit(100);
-printf ("%d\n", (get_op_func)argv[2](atoi(argv[1]), atoi(argv[3])))
-return (0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", (*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
