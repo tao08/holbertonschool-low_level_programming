51efe0efb80a7dff0c094a3a7088191106c64147
tao08 <1823@holbertonschool.com>
2020-07-24 23:17:14 +0000

    constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include"lists.h"
+/**
+ * first - print first that main, la liebre
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
