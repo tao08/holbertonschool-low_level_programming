304a9dcd54168ba01dbc4243fff4842b03102467
tao08 <1823@holbertonschool.com>
2020-07-29 22:39:11 +0000

    add_new_nudo

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *nuevo;
 
-	new = malloc(sizeof(listint_t));
+	nuevo = malloc(sizeof(listint_t));
 	if (nuevo == NULL)
 	{
 		return (NULL);
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,7 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 
-listint_t *add_nodeint(listint_t **head, const int n);nt _putchar(char c);
+listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 
