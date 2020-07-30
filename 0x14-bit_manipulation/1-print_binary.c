9ef4d6a87b3a49e089022fe35ff58a207e77cc03
tao08 <1823@holbertonschool.com>
2020-07-30 23:27:32 +0000

    binari

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_binary - Write a function that prints the binary representation g
+ * @n: the number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		putchar ((n & 1) + '0');
+	}
+	else
+	{
+		putchar (n + '0');
+	}
+}
