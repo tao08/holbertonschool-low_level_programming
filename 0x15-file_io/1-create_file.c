ffa1dd12a5bd0e5ad365ca5ff6a958d917757272
tao08 <1823@holbertonschool.com>
2020-08-06 03:06:24 +0000

    file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ *create_file - create a file
+ * @filename: filename
+ * @text_content: text content
+ * Return: create file
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int count, contex, tenor = 0;
+	if (filename == NULL)
+	{
+		return (-1);
+
+	}
+	count = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (count == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[tenor])
+		{
+			tenor++;
+		}
+	}
+	contex = write(count, text_content, tenor);
+
+	if (contex == -1)
+	{
+		return (-1);
+		close(count);
+	}
+	return (1);
+}
