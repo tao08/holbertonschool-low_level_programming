16367cf86fcc33854e45760a42a06c173dc5d85e
tao08 <1823@holbertonschool.com>
2021-02-20 04:31:01 +0000

    py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
+""" Module """
 
 
 def island_perimeter(grid):
-    """Python Function to print perimeter of an island"""
+    """ island_perimeter method """
     perimeter = 0
-    for line in grid:
-        perimeter += line.count(1) * 4
-    for line in grid:
-        for i in range(len(line)-1):
-            if line[i] == 1 and line[i+1] == 1:
-                perimeter -= 2
-
-    for i in range(len(grid)-1):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1 and grid[i+1][j] == 1:
-                perimeter -= 2
-
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                top = i - 1
+                down = i + 1
+                left = j - 1
+                rigth = j + 1
+                if i == 0 or grid[top][j] == 0:
+                    perimeter += 1
+                if i + 1 == len(grid) or grid[down][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][left] == 0:
+                    perimeter += 1
+                if j + 1 == len(grid[i]) or grid[i][rigth] == 0:
+                    perimeter += 1
     return perimeter
